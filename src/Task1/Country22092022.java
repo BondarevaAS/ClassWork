36a8af3d92484d07d82876365312d76fc8048aa8
vladdi9 <am4elvlad@gmail.com>
2022-09-22 21:18:04 +0300

    new classw2209

---
+++ src/Task1/Country22092022.java
@@ -0,0 +1,16 @@
+package Task1;
+
+import by.step.teach_java.tack3.Country;
+
+import java.util.Scanner;
+
+public abstract class Country22092022 {
+    private String nameOfCountry;
+    private String countryOfContinent;
+    public Country22092022(String nameOfCountry){
+      this.nameOfCountry=nameOfCountry;
+    }
+    public Country22092022(String nameOfCountry, String nameOfCountry){
+
+    }
+}
